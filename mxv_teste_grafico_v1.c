2877d2efbad46903d0e180e2ceaa8fd084b52f95
arleswasb <werbert.barradas.390@ufrn.edu.br>
2025-08-15 10:18:57 -0300

    Update mxv_teste_grafico_v1.c

--- mxv_teste_grafico_v1.c
+++ mxv_teste_grafico_v1.c
@@ -117,7 +117,7 @@ int main() {
             fprintf(stderr, "Falha ao alocar para N=%d\n", N);
             continue;
         }
-        
+        // Inicialização de dados na matriz e no vetor
         for(int i=0; i<M; i++) for(int j=0; j<N; j++) A[i][j] = 1.0;
         for(int i=0; i<N; i++) x[i] = 1.0;
 
